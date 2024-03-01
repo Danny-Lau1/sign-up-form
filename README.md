1ee04bf47fdbb179b7b0669b266784c2c985bbc0
Danny <dannylau@Dannys-MacBook-Air.local>
2024-02-29 16:59:50 -0800

    fix link and add password match

--- README.md
+++ README.md
@@ -3,6 +3,4 @@
 
 This a simple form page using html and css. The layout was accomplished using CSS flexbox. Minimal validation is also incorporated in the form element.
 
-### Live Preview
-
-https://danny-lau1.github.io/sign-up-form/
+## Live Demo HERE: [Sign Up Form](https://danny-lau1.github.io/sign-up-form/)
---
+++ script.js
@@ -0,0 +1,13 @@
+let initial_password = document.querySelector("#password")
+let confirm_password = document.querySelector("#confirm-password")
+
+
+let matching = document.querySelector(".matching")
+
+
+if (initial_password.textContent === "" && initial_password.textContent === confirm_password.textContent) {
+    matching.textContent = "Passwords Match!"
+}
+else {
+    matching.textContent = "Passwords Do Not Match";
+}
